4dcfd584807bf0c077e71006f15945842dea21b7
Bruno <brnyza@gmail.com>
2022-02-01 11:15:37 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+
+async function main() {
+  const msg = 'Teste Git'
+  const url = `https://api.telegram.org/bot1221548327:AAHe6kWjxuly7W_uARM6giFIXsIxyk6CXI4/sendMessage?chat_id=1064217093&text=${msg}`
+  const retorno = await axios.get(url)  
+  console.log(retorno.data)
+}
+
+main()
