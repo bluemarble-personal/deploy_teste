a0272379ef176a26d92c787fd61425d66228dd96
Bruno <brnyza@gmail.com>
2022-02-01 11:46:59 -0300

    teste 123

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 async function main() {
-  const msg = 'Teste Git'
+  const msg = 'Teste Git 123'
   const url = `https://api.telegram.org/bot1221548327:AAHe6kWjxuly7W_uARM6giFIXsIxyk6CXI4/sendMessage?chat_id=1064217093&text=${msg}`
   const retorno = await axios.get(url)  
   console.log(retorno.data)
